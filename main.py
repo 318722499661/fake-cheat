c21222137954e58fb4c8bab493c10b5767b92e2d
Garbg <83542885+CloudyWhale@users.noreply.github.com>
2022-08-11 12:22:15 -0400

    Add slider for autoclicker

--- main.py
+++ main.py
@@ -8,20 +8,23 @@ def is_over(rect, pos):
     return True if rect.collidepoint(pos[0], pos[1]) else False
 
 
-class slider:
+class Slider:
     def __init__(self):
-        print("banana")
         self.width = 255
         self.posx = 50
         self.posy = 175
-        value = 0
+        self.value = 0
+        self.steps = 20
+        self.slidername = "MinCPS:"
+        self.value = 0.0
 
-        def rederSlider():
-            sliderrect = screen.fill((49, 50, 68), (self.posx, self.posy, self.width, 25))
-            screen.fill(accent, (50, 175, value, 25))
-            screen.blit(redtext, (10, 175))
-            numtext = font.render(str(accent[0]), True, (255, 255, 255))
-            screen.blit(numtext, ((self.posx + self.width) + 50, 175))
+    def renderSlider(self):
+        self.sliderrect = screen.fill((49, 50, 68), (self.posx, self.posy, self.width, 25))
+        screen.fill(accent, (self.posx, self.posy, self.value / (self.steps / self.width), 25))
+        slidernametext = font.render(self.slidername, True, (255, 255, 255))
+        screen.blit(slidernametext, (10, self.posy))
+        numtext = font.render(str(self.value), True, (255, 255, 255))
+        screen.blit(numtext, ((self.posx + self.width) + 25, self.posy))
 
 
 minecraft_running = os.popen('pgrep -a java | grep "minecraft"')
@@ -29,12 +32,12 @@ minecraft_running = os.popen('pgrep -a java | grep "minecraft"')
 if minecraft_running.read() == "":
     print("minecraft not running")
     minecraft_running.close()
-    pygame.quit()
-    exit()
+    #pygame.quit()
+    #exit()
 else:
     print("minecraft is running")
 
-slider = slider()
+
 minecraft_running.close()
 tab = 1
 cheatname = "Autoclicker"
@@ -64,7 +67,12 @@ redslider = screen.fill((49, 50, 68), (50, 175, 256, 25))
 greenslider = screen.fill((49, 50, 68), (50, 225, 256, 25))
 blueslider = screen.fill((49, 50, 68), (50, 275, 256, 25))
 
-
+mincps = Slider()
+mincps.posx = 100
+maxcps = Slider()
+maxcps.posx = 100
+maxcps.posy = 225
+maxcps.slidername = "MaxCPS:"
 def rederRedSlider():
     redslider = screen.fill((49, 50, 68), (50, 175, 255, 25))
     screen.fill((accent), (50, 175, accent[0], 25))
@@ -107,17 +115,26 @@ while True:
         cheatsenabledtext = font.render("Enabled: " + str(cheatsenabled[cheatname.lower()]).lower(), True,
                                         (255, 255, 255))
         cheatsenabledrect = screen.blit(cheatsenabledtext, (10, 125))
-    if tab == 4:
+
+    if tab == 1:
+        mincps.renderSlider()
+        maxcps.renderSlider()
+    elif tab == 4:
         screen.blit(guicolortext, (10, 125))
         rederRedSlider()
         renderGreenSlider()
         renderBlueSlider()
     if pygame.mouse.get_pressed()[0]:
-        if is_over(redslider, pygame.mouse.get_pos()):
+        if is_over(mincps.sliderrect, pygame.mouse.get_pos()) and tab == 1:
+            mincps.value = round((pygame.mouse.get_pos()[0] - mincps.posx) * (mincps.steps / mincps.width), 2)
+
+        if is_over(maxcps.sliderrect, pygame.mouse.get_pos()) and tab == 1:
+            maxcps.value = round((pygame.mouse.get_pos()[0] - maxcps.posx) * (maxcps.steps / maxcps.width), 2)
+        elif is_over(redslider, pygame.mouse.get_pos()) and tab == 4:
             accent = ((pygame.mouse.get_pos()[0] - 50), accent[1], accent[2])
-        elif pygame.mouse.get_pressed()[0] and is_over(greenslider, pygame.mouse.get_pos()):
+        elif pygame.mouse.get_pressed()[0] and is_over(greenslider, pygame.mouse.get_pos()) and tab == 4:
             accent = (accent[0], (pygame.mouse.get_pos()[0] - 50), accent[2])
-        elif pygame.mouse.get_pressed()[0] and is_over(blueslider, pygame.mouse.get_pos()):
+        elif pygame.mouse.get_pressed()[0] and is_over(blueslider, pygame.mouse.get_pos()) and tab == 4:
             accent = (accent[0], accent[1], (pygame.mouse.get_pos()[0] - 50))
     pygame.display.update()
     for event in pygame.event.get():
